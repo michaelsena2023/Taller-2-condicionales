75cf20fb05f6ca34f99a9128a22da2b717e05eab
michaelsena2023 <132312187+michaelsena2023@users.noreply.github.com>
2023-05-09 20:51:18 -0500

    Create main(copy).py

---
+++ main(copy).py
@@ -0,0 +1,177 @@
+#inputs
+number=int(input(^Ingrese un numero entero:^))
+
+#process y outputs
+if (number >0):
+  print(fÊl numero (number)es positivo^)
+else:
+  print (^El numero ^ + str(number) + ^  es negativo^
+  continuation point 19
+  number 1=int (input(ingrese numero 1^^ ()
+  number 2=   ^^ (  ^^ (^^ ^^ ^^ 2: 2 ^^)
+  number 3=  ^^(  ^^ ( ^^  ^^ ^^ 3: ^^))
+  if(number 1 > number 2 number 1 > number 3 )
+  print (^^ the number ^^ , number 1 , ^^ is the eldest ^^)
+elif (number 2 > number 1 number 2 >number 3 )
+print (^^ the number ^^ , number 2 ^is the eldest^^)
+else
+print (^^ the number ^^ , number 3 , ^^ is the eldest^^)
+continuation point twenty
+print(employee salary)
+name =input(type your name:^^)
+normal_hours=int(input(digite cuantas horas trabja normalmente:^^))
+extra hours=int(input(^^ digite cuantas horas extras trabaja:^^))
+#process
+salary_hour=5
+salary_normal=normal_hours*salary_hour
+salari..hour
+salary_extra=salary_normal+salary_extra
+#outputs
+print(the normal salary of name is: total_salary,^^dolars^^)
+exercice 4
+print
+#inputs 
+#Process
+if a <b : 
+  result a + b 
+else : 
+  result=a.b
+  #outputs
+  print the result is: ^^result)
+  exercice 21
+  print
+  #inputs
+  a=float(inputingreseelprimernumero:^^))
+  b=float(inputingreseelsegundonumero:^^))
+  #Process
+  ifb==0:
+  print(noesposibledividirpor0)
+elif a ==b:
+print(el cociente de la division es : 1^^)
+  else:
+quotient=a/b
+#outputs
+print (el cociente de la division es  ^^ , quptient)
+exercice 22 
+print
+numbia=int(input(input(ingrese un numero del  1 al 6 : ^^ ))
+#Process
+ifnumdia==1:
+print(^^lunes ^^)
+elifnumdia==2:
+  print(tuestay^^)
+  elif numdia ==3
+print(wednesday^^)
+elifnumdia==4
+print(thrusday^^)
+ elifnumdia==5
+print(^^friday)
+elifnumdia==6
+#outyputs
+else:
+  print ( el numero ingresado no es valido)
+exercice 23 
+print (^^Triàngulo equilàtero)
+#inputs
+side 1 =float(input(ingrese la longitud del primer lado:^^))
+side2=float(input(^^ ingrese la longitud del segundo lado:^^))
+side 3=float(input(^^ ingrese la longitud del tercer lado:^^))
+#Process
+if side1==side2andside2==side3:
+print (^^el triàngulo equilàtero^^)
+#outputs
+  else:
+print(^^El triàngulo no es equilàtero )
+exercise 24 
+  print(welcome)
+#inputs
+number1=int(input(ingrese el primer numero:^^))
+number2=int(input(ingrese el segundo numero:^^))
+#Process
+ifnumber1<0 or number2< 0:
+result=number1+number2
+print(el resultado es result)
+exercise25
+print (welcome)
+#inputs 
+day=int(input(^^ introduce el dia de nacimiento (03 -07):^^))
+month=int(input(introduce el mes de nacimiento (07_03):^^)
+#Process
+itday==1:
+ifday<20
+sign=^^piscis^^
+elifmonth==2
+  ifday<19:
+sign:piscis
+elifmonth==3
+if day < 21:
+sign=^^piscis^^
+else:
+sign=^^piscis^^
+elif month==4:
+ifday<20:
+sign=^^piscis^^
+else:
+sign=^^tauro^^
+  elif month==5:
+if day <21:
+sign=^^escorpion^^
+else:
+sign=^^leo^^
+else:
+sign=^^acuario^^
+elif month==9
+if day < 23:
+sign=^^libra^^
+else:
+sign:^^escorpio^^
+elif month==10:
+if day < 23:
+sign=^^libra^^
+else:
+sign=^^escorpio^^
+elif month==11
+if day <22
+sign=^^libra^^
+else :
+sign=^^capricornio^^
+#outputs
+print(tu signo es:, sign)
+exercise 27
+print(welcome)
+#inputs
+base=int(input(ingrese el valor de la base:^^))
+high=int(input(ingrese el valor de la altura:^^))
+#Process
+if base==high:
+figure=^^cuadrade^^
+else:
+figure=^^rectàngle^^
+perimeter=2*(base*high)
+surface=base*high
+#outputs:
+print(the cuadrilate is one: , figure)
+print(the perimeter is:^^ , perimeter)
+printthe surface is:^^ , surface)
+exercise 28 
+print (welcome)
+#inputs
+#process
+if selling_price<100:
+discount=selling_price*0.05
+  elif selling _price*0.10
+else:
+discount=selling_price-discount
+#outputs
+print(the discount made is $^^, discount)
+print(the final discounted price is$^^,final-price)
+exercise 29 
+print (welcome)
+#inputs
+year=int(input(enter a year to determine if it is a leap year or not:^^))
+#process
+if(year%4==0andyear%100!=0)or(year%400==0):
+print(year,^^it's a leap year^^)
+#outputs
+else:
+  print(^^not a leap year^^)
